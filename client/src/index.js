afb6a9133b636f099e1fd07beb3ca07e72cfac95
Ali Ahmad <alislearning123@gmail.com>
2021-06-13 18:10:05 -0400

    Uninstall Redux Packages & Remove Initial Redux Setup

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
-import { Provider } from 'react-redux';
-import store from './redux/store';
 
 ReactDOM.render(
   <ChakraProvider>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </ChakraProvider>,
   document.getElementById('root')
 );
