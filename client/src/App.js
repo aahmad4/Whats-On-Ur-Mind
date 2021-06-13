e3e5d121f6ab0afb026f7debfc3dd8fbb433bae0
Ali Ahmad <alislearning123@gmail.com>
2021-06-12 22:25:58 -0400

    Set Up Chakra UI & Install Additional React Dependencies (Router / Redux) & Create Base Navbar Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 
 export default function App() {
-  return <div>Why isn't the font different :(</div>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
