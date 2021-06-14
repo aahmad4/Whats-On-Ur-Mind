b15672369416ba2cd90598a40716b7f7bf49dacf
Ali Ahmad <alislearning123@gmail.com>
2021-06-14 16:08:30 -0400

    Fix Heroku Port

--- wsgi.py
+++ wsgi.py
@@ -4,13 +4,7 @@ from server.db import db
 from waitress import serve
 
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
 
-if os.getenv('FLASK_ENV') != 'development':
-    db.init_app(app)
-    serve(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
-else:
-    db.init_app(app)
-    app.run(port=5000, debug=True) 
+db.init_app(app)
+app.run(host='0.0.0.0', port=int(os.getenv('PORT', 33507))) 
