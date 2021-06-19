265c896134019ede437dd6c59cf586c66ae5842b
Ali Ahmad <alislearning123@gmail.com>
2021-06-19 19:28:48 -0400

    Enable Users To Delete Questions Via Their Dashboard

--- client/src/components/DashboardScreen/QuestionCard.jsx
+++ client/src/components/DashboardScreen/QuestionCard.jsx
@@ -9,6 +9,7 @@ import {
   Button,
   Textarea,
   IconButton,
+  useToast,
 } from '@chakra-ui/react';
 import { EditIcon, ChevronUpIcon, DeleteIcon } from '@chakra-ui/icons';
 import TimeAgo from 'timeago-react';
@@ -16,6 +17,8 @@ import TimeAgo from 'timeago-react';
 export default function QuestionCard({ question, userDetails }) {
   const [answerOpen, setAnswerOpen] = useState(false);
   const [answerText, setAnswerText] = useState('');
+
+  const toast = useToast();
 
   const answerQuestion = async () => {
     try {
@@ -50,6 +53,42 @@ export default function QuestionCard({ question, userDetails }) {
     }
   };
 
+  const deleteQuestion = async () => {
+    try {
+      if (window.confirm('Are you sure?')) {
+        const { data } = await axios.post(
+          '/api/users/refresh',
+          {},
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${userDetails.get().refresh_token}`,
+            },
+          }
+        );
+
+        const { data: deleteData } = await axios.delete(
+          `/api/questions/${userDetails.get().username}/${question.id}`,
+          {
+            headers: {
+              Authorization: `Bearer ${data.access_token}`,
+            },
+          }
+        );
+
+        toast({
+          title: 'Success',
+          description: deleteData.message,
+          status: 'success',
+          duration: 9000,
+          isClosable: true,
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <Box p={4} bg="gray.100" borderRadius="lg" mb={5}>
       <Box mt={{ base: 4, md: 0 }}>
@@ -64,6 +103,7 @@ export default function QuestionCard({ question, userDetails }) {
           New Question
         </Badge>
         <IconButton
+          onClick={deleteQuestion}
           size="sm"
           float="right"
           mb={2}
