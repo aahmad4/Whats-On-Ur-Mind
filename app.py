98cfadc3f7cda19a7406fa52780695c9c4ce5f08
Ali Ahmad <alislearning123@gmail.com>
2021-06-25 22:53:40 -0400

    Add Pycache Directory To .gitignore

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from server.main import app
 from server.db import db
+from server.db import migrate
 
 import os
 
 db.init_app(app)
+migrate.init_app(app, db)
 
 if __name__ == "__main__":
     app.run(port=int(os.getenv('PORT', 5000)), debug=True)
