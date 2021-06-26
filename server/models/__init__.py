46414dd35ff9c87d7d6786afae841ba8a50e047d
Ali Ahmad <alislearning123@gmail.com>
2021-06-25 23:22:59 -0400

    Do Some Quick Database Migrations

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -2,4 +2,8 @@
 Initial command executed in 'server' directory to create migration directory
 
 pipenv run flask db init --directory server/models/migrations
+
+pipenv run flask db migrate -d server/models/migrations
+
+pipenv run flask db upgrade -d server/models/migrations
 """
