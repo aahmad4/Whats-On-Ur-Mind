50b9f415433bad9e4dd44e8fdd93505e29042c02
Ali Ahmad <alislearning123@gmail.com>
2021-06-25 23:34:03 -0400

    Add Temporary Database Column To Questions Table To Test DB Migrations In Production

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -3,6 +3,8 @@ Initial command executed in 'server' directory to create migration directory
 
 pipenv run flask db init --directory server/models/migrations
 
+pipenv run flask db stamp head -d server/models/migrations
+
 pipenv run flask db migrate -d server/models/migrations
 
 pipenv run flask db upgrade -d server/models/migrations
